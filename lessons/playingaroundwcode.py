23559fc538bcb937a535f13f75deb71b3e64fe29
Henry Rowen <hrowen@unc.edu>
2024-08-26 21:06:33 -0400

    Editted "playingaroundwithcode.py"

--- lessons/playingaroundwcode.py
+++ lessons/playingaroundwcode.py
@@ -1,15 +1,18 @@
 """My file to play around with python"""
 
+import math
+
 
 def sqrfinder(x: int) -> str:
     """Finds a close approximation to the square root of x"""
     g = x
     while ((g**2) - x) > 0.000000001:
         g = (g + x / g) / 2
-        print(str(g))
         if ((g**2) - x) <= 0.000000001:
             break
     return f"The square root of x is approx. {g}"
 
 
 print(sqrfinder(x=16))
+
+print(math.sqrt(16))
